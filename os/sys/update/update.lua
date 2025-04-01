80adf65152a887c6f2a2d5445f936da8c5f8fea3
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2025-03-31 18:47:54 -0600

    fixed ver again

--- os/sys/update/update.lua
+++ os/sys/update/update.lua
@@ -19,7 +19,8 @@ function getWebRaw(url)
 end
 
 function check()
-    local url = url_template .. "/" .. branch .. "/os/ver.lua"
+    local url = url_template .. branch .. "/os/ver"
+    local url = "https://raw.githubusercontent.com/" .. user .. "/" .. repo .. "/" .. branch .. "/os/ver"
     local cloud_version = tonumber(getWebRaw(url))
     local local_version = tonumber(version)
     return cloud_version > local_version
