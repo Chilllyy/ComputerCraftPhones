2f01c36c6290da5d7aaceebdd7fe167556de8c74
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2025-03-31 19:22:33 -0600

    fixed update

--- os/sys/update/update.lua
+++ os/sys/update/update.lua
@@ -55,6 +55,7 @@ function update()
 
     if pcall(clone, url, folder) then
         delete("/startup.lua")
+        delete("/startup")
         fs.makeDir("/startup")
         fs.copy("/os/sys/update/startup.lua", "/startup/install.lua")
         delete("/os")
