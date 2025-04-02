21c27fc273dc15764039573dea98f286c50b8198
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2025-04-01 19:08:26 -0600

    Fixed Update

--- os/sys/update/update.lua
+++ os/sys/update/update.lua
@@ -6,13 +6,6 @@ local repo = "ComputerCraftPhones"
 local branch = settings.get("upd_branch") or "stable"
 local url_template = "https://api.github.com/repos/" .. user .. "/" .. repo .. "/"
 
-function getWebTable(url)
-    local response = http.get(url)
-    local data = response.readAll()
-    local table = textutils.unserializeJSON(data)
-    return table
-end
-
 function getWebRaw(url)
     local response = http.get(url)
     return response.readAll()
@@ -26,39 +19,14 @@ function check()
     return cloud_version > local_version
 end
 
-function clone(url, folder)
-    fs.makeDir(folder)
-    response = http.get(url)
-    data = textutils.unserializeJSON(response.readAll())
-    for i,v in ipairs(data) do
-        if v.type == "dir" then
-            local new_url = v.url
-            local new_dir = folder .. "/" .. v.name
-            clone(new_url, new_dir)
-        elseif v.type == "file" then
-            local dl_url = v.download_url
-            local dl_r = http.get(dl_url)
-            local dl_d = dl_r.readAll()
-            local file = folder .. "/" .. v.name
-            local f = fs.open(file, "w")
-            f.write(dl_d)
-            f.close()
-        end
-    end
-end
-
 function update()
-    local url = url_template .. "contents?ref=" .. branch
-    local folder = "/tmp/upd"
-
-    fs.delete(folder)
-
-    clone(url, folder)
+    fs.delete("/tmp/upd")
 
     delete("/startup.lua")
     fs.makeDir("/startup")
     fs.copy("/os/sys/update/startup.lua", "startup/install.lua")
     delete("/os")
+    os.sleep(2)
     os.reboot()
 end
 
@@ -74,6 +42,7 @@ end
 print("Checking for Updates...")
 if check() then
     print("Found Update")
+    print("Please Wait...")
     update()
 else
     print("No Update found!")
